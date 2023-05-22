0a4190ce120f98d2b36ddfbfe98e9343121740eb
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-05-22 11:13:53 +0200

    authors added

---
+++ AUTHORS.md
@@ -0,0 +1,4 @@
+# Authors
+
+- [Kingsley B. Boafo]( https://github.com/kayc0des ) - SE Student
+- [Davis Gatabazi]( https://github.com/therealgatabazi ) - SE Student
---
+++ generate_authors.py
@@ -0,0 +1,33 @@
+#Python File to Generate Authors
+
+#List of dicts containing authors
+authors = [
+    {
+        'name': 'Kingsley B. Boafo',
+        'email': 'k.boafo@alustudent.com',
+        'github': 'kayc0des',
+        'role': 'SE Student'
+    },
+    {
+        'name': 'Davis Gatabazi',
+        'email': 'd.gatabazi@alustudent.com',
+        'github': 'therealgatabazi',
+        'role': 'SE Student'
+    },
+]
+
+#function to generate the Markdown file content based on the authors' information
+def generate_authors_file(authors):
+    content = '# Authors\n\n'
+    
+    for author in authors:
+        github_link = f'https://github.com/{author["github"]}'
+        content += f'- [{author["name"]}]( {github_link} ) - {author["role"]}\n'
+
+    return content
+
+#Call the generate_authors_file() function with the authors list and write the output to a file
+output_file = 'AUTHORS.md' 
+
+with open(output_file, 'w') as file:
+    file.write(generate_authors_file(authors))
