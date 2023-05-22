4c02898129ce762e2a5ba003b1e91d46e8391820
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-05-22 14:24:56 +0200

    test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-import sys
-import os
-
-# Get the absolute path of the directory containing test_base_model.py
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Get the absolute path of the models directory
-models_dir = os.path.join(script_dir, '../models')
-
-# Add the models_dir to sys.path
-sys.path.append(models_dir)
-
-from models.base_model import BaseModel
-
+from base_model import BaseModel
 
 
 my_model = BaseModel()
