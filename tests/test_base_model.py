8aa06be5fb152855d6646835d16fb858dd929109
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-05-23 13:56:18 +0200

    test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -16,32 +16,32 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
-        self.my_model = BaseModel()
+        self.model = BaseModel()
 
     def test_id_is_string(self):
-        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.model.id, str)
 
     def test_created_at_is_datetime(self):
-        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.model.created_at, datetime)
 
     def test_updated_at_is_datetime(self):
-        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_save_updates_updated_at(self):
-        initial_updated_at = self.my_model.updated_at
-        self.my_model.save()
-        self.assertNotEqual(initial_updated_at, self.my_model.updated_at)
+        initial_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(initial_updated_at, self.model.updated_at)
 
     @patch('base_model.datetime')
     def test_to_dict_returns_correct_dictionary(self, mock_datetime):
         mock_datetime.now.return_value = datetime(2023, 5, 1, 10, 30)
         expected_dict = {
             '__class__': 'BaseModel',
-            'id': self.my_model.id,
-            'created_at': self.my_model.created_at.isoformat(),
-            'updated_at': self.my_model.updated_at.isoformat()
+            'id': self.model.id,
+            'created_at': self.model.created_at.isoformat(),
+            'updated_at': self.model.updated_at.isoformat()
         }
-        self.assertDictEqual(self.my_model.to_dict(), expected_dict)
+        self.assertDictEqual(self.model.to_dict(), expected_dict)
 
 
 if __name__ == '__main__':
